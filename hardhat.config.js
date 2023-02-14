2f852d55e2c793adf281c3516736e092ed88653b
shu <shu@unifra.io>
2023-02-14 18:07:24 +0800

    tmp commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,5 +18,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  //  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      "optimizer": {
+        "enabled": true,
+        "runs": 200
+      },
+      outputSelection: {
+        "*": {
+          "": [],
+          "*": ["abi", "metadata", "devdoc", "userdoc", "storageLayout", "evm.legacyAssembly", "evm.methodIdentifiers"]
+        }
+      }
+    }
+  }
 };
