88d8ac57e65e8fb1e3a31ec9062062e5c79c0ec1
shu <shu@unifra.io>
2023-02-22 10:55:22 +0800

    1. opCodeName DIFFICULTY==PREVRANDAO 2. fix cmpVersion bug

--- hardhat-contract-compat-check/index.js
+++ hardhat-contract-compat-check/index.js
@@ -46,8 +46,8 @@ task(
     let buff = fs.readFileSync(path);
     buildInfo = JSON.parse(buff);
     let { solcVersion } = buildInfo;
-    if (solcVersion < "0.6.3") {
-      console.log('solcVersion', solcVersion, "not support compatibility checking.")
+    if (cmpVersions(solcVersion, "0.6.3") < 0) {
+      console.log('solcVersion', solcVersion, "not support compatibility checking. Need solc version >=0.6.3")
       return;
     }
     let contracts = buildInfo["output"]["contracts"];
@@ -240,4 +240,20 @@ function l2Support(l2Name) {
       throw err;
     }
   }
+}
+
+function cmpVersions(a, b) {
+  var i, diff;
+  var regExStrip0 = /(\.0+)+$/;
+  var segmentsA = a.replace(regExStrip0, '').split('.');
+  var segmentsB = b.replace(regExStrip0, '').split('.');
+  var l = Math.min(segmentsA.length, segmentsB.length);
+
+  for (i = 0; i < l; i++) {
+    diff = parseInt(segmentsA[i], 10) - parseInt(segmentsB[i], 10);
+    if (diff) {
+      return diff;
+    }
+  }
+  return segmentsA.length - segmentsB.length;
 }
